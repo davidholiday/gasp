9434ce06f57ae8cf307b6f732405b46623347015
snerd <you@example.com>
2020-04-26 15:21:32 -0600

    checkpoint

--- cpp/hw_1/hw/main.cpp
+++ cpp/hw_1/hw/main.cpp
@@ -1,16 +1,50 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <cstdlib> // for EXIT_SUCCESS and EXIT_FAILURE
 #include <boost/program_options.hpp>
 
-int main(int argc, char **argv) {
+
+int parse_program_options(int argc, char **argv) {
 	namespace po = boost::program_options;
     po::options_description desc{"Allowed options"};
+
+    
     desc.add_options()
-        ("help", "display help message")
+        ("help,h", "display help message")
         ("floor", po::value<int>(), "floor of password size to generate")
         ("ceiling", po::value<int>(), "non-inclusive ceiling of password size to generate")
     ;
     
+    po::positional_options_description floor_positional_option_description;
+    floor_positional_option_description.add("floor", 0);
+    
+    po::positional_options_description ceiling_positional_option_description;
+    ceiling_positional_option_description.add("ceiling", 1);
+    
+    po::command_line_parser parser{argc, argv};
+    parser.options(desc).positional(floor_positional_option_description).allow_unregistered();
+    parser.options(desc).positional(ceiling_positional_option_description).allow_unregistered();
+    
+    po::parsed_options parsed_options = parser.run();
+    
+    po::variables_map vm;
+    po::store(parsed_options, vm);
+    po::notify(vm);
+    
+    if (vm.count("help")) {
+        std::cout << desc << '\n';
+    }
+    
+    
+    
 	return EXIT_SUCCESS;
 }
+
+
+int main(int argc, char **argv) {
+    int parse_program_options_rc = parse_program_options(argc, argv);
+    return parse_program_options_rc;
+}
+
+
 
