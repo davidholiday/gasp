92cc4585be16b26c445853c82e18fe2abd6e1070
snerd <you@example.com>
2020-04-26 21:53:48 -0600

    not bad

--- cpp/hw_1/hw/main.cpp
+++ cpp/hw_1/hw/main.cpp
@@ -4,25 +4,24 @@
 #include <boost/program_options.hpp>
 
 
-int parse_program_options(int argc, char **argv) {
+#define OUT
 
+
+int parse_program_options(int argc, char **argv, OUT int& floorOut, OUT int& ceilingOut) {
+
+    int rc = EXIT_SUCCESS;
     namespace po = boost::program_options;
     
     std::string desc_txt = "Description:\n"
                            "generates all possible combinations of printable characters, rates them with zxcvbn, and "
                            "serializes by length and rating.";
-    
-    // for holding onto positional argument values 
-    int floor_value;
-    int ceiling_value;
-
-    
+        
     // named argument descriptions
     po::options_description desc{"Allowed options"};    
     desc.add_options()
         ("help,h", "display help message") 
-        ("floor,f", po::value<int>(&floor_value), "(required) floor of password size to generate")
-        ("ceiling,c", po::value<int>(&ceiling_value), "(required) non-inclusive ceiling of password size to generate")
+        ("floor,f", po::value<int>(&floorOut), "(required) floor of password size to generate")
+        ("ceiling,c", po::value<int>(&ceilingOut), "(required) non-inclusive ceiling of password size to generate")
     ;
 
     
@@ -36,25 +35,36 @@ int parse_program_options(int argc, char **argv) {
         po::store(parsed, vm);
         po::notify(vm);
         
+        
+        // validation 
+        if ( !vm.count("floor") || !vm.count("ceiling") ) {
+            throw std::invalid_argument("both floor and ceiling parameters must be specified");
+        }
+
+        if (floorOut < 1) {
+            throw std::invalid_argument("floor must be greater than 0 ");
+        }
+        
+        if (ceilingOut <= floorOut) {
+            throw std::invalid_argument("ceiling value must be greater than floor value");
+        }
+        
+        
+        // respond
         if (vm.count("help")) {
             std::cout << desc_txt << "\n\n";
             std::cout << desc << "\n";
         }      
 
-        if ( !(vm.count("floor") && vm.count("ceiling")) ) {
-            throw std::invalid_argument("both floor and ceiling parameters must be specified");
-        }
-
-
- 
     } catch (std::exception &ex) {
         std::cerr << ex.what() << "\n\n";
         std::cout << desc_txt << "\n\n";
         std::cout << desc << "\n";
+        rc = EXIT_FAILURE;
     }
 
 
-	return EXIT_SUCCESS;
+	return rc;
 }
 
 
@@ -63,8 +73,18 @@ int parse_program_options(int argc, char **argv) {
 
 
 int main(int argc, char **argv) {
-    int parse_program_options_rc = parse_program_options(argc, argv);
-    return parse_program_options_rc;
+    int rc = EXIT_SUCCESS;
+    
+    int floor, ceiling;
+    int parse_program_options_rc = parse_program_options(argc, argv, floor, ceiling);
+    
+    if (parse_program_options_rc == EXIT_FAILURE) {
+        rc = EXIT_FAILURE;
+    } else {
+        std::cout << "floor value is: " << floor << "   ceiling value is: " << ceiling << "\n";
+    }
+    
+    return rc;
 }
 
 
